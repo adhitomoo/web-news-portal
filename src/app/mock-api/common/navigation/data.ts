266101ff41d41eb1e3ee9e6e607e4da591b6a55a
adhitomoo <adhitomo22@gmail.com>
2024-08-01 23:46:39 +0700

    add menus and layout

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -3,10 +3,38 @@ import { FuseNavigationItem } from '@fuse/components/navigation';
 
 export const defaultNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
-        title: 'Example',
-        type : 'basic',
+        id   : 'dashboard',
+        title: 'Dashboard',
+        type : 'aside',
         icon : 'heroicons_outline:chart-pie',
+        link : '/example'
+    },
+    {
+        id   : 'caategories',
+        title: 'Category',
+        type : 'aside',
+        icon : 'heroicons_outline:rectangle-group',
+        link : '/example'
+    },
+    {
+        id   : 'articles',
+        title: 'Article',
+        type : 'aside',
+        icon : 'heroicons_outline:newspaper',
+        link : '/example'
+    },
+    {
+        id   : 'tags',
+        title: 'Tags',
+        type : 'aside',
+        icon : 'heroicons_outline:tag',
+        link : '/example'
+    },
+    {
+        id   : 'ads',
+        title: 'Ads',
+        type : 'aside',
+        icon : 'heroicons_outline:tv',
         link : '/example'
     }
 ];
